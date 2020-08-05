2ba3fc1110540fc594cca17be796752cfda92628
Kyaara <7339.goyal@gmail.com>
2020-08-05 13:01:36 -0500

    vanishing pigs

--- BaseClass.js
+++ BaseClass.js
@@ -13,11 +13,11 @@ class BaseClass{
       }
       display(){
         var angle = this.body.angle;
-        push();
+        push(); //adding a new setting
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
         imageMode(CENTER);
         image(this.image, 0, 0, this.width, this.height);
-        pop();
+        pop(); //revert back to the old setting without translate & rotate
       }
 }
